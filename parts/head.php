ccd1f73a0b5b787a651a998211e4f08814038aaa
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-11-20 19:19:57 +0900

    Create head.php

---
+++ parts/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<meta charset="UTF-8">
+<title>それ見た</title>
+<link rel="stylesheet" href="styles.css"/>
+</head>
+
+<div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
+    <h3>ｼｪｱｱﾆ</h3></br>
+</div>
