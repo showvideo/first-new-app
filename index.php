7f2f08bb9d496a924d41ee3fd38c347e2f8abb0b
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-11-21 18:31:22 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
     require('func/function.php')
 ?>
 
-こｆｂｐｂｐｄばｆｄ
-
 <div style = "display:inline-block;border:ridge;margin-left:405px;width:450px;height:1000px;">
     あなたが見たｱﾆﾒを共有しよう</br>
     ｱﾆﾒで繋がる掲示板</br>
