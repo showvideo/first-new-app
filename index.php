bf61b325917b4864099054e41eac89d1514767b1
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-11-21 18:07:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -28,9 +28,7 @@ echo "ﾃﾞｲ氏ね";
 <div style = "display:inline-block;vertical-align:top;border:ridge;height:700px;width:200px;">
     最新ｱﾆﾒのTL</br>
     <div>
-        <?php
-        require('parts/tl.php')
-        ?>
+
     </div>
 
 </div>
