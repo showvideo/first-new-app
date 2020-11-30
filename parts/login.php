0132e2ad9f07374d5d515244e69a66eefc465b4c
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-01 01:54:25 +0900

    Update login.php

--- parts/login.php
+++ parts/login.php
@@ -1,1 +1,23 @@
-<p>ﾃﾞｲはよやめて自分の夢を追いかけたい</P>
+<?php
+    require('../head.php');
+?>
+
+<?php
+    require('../func/function.php')
+?>
+
+<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
+
+    </div>
+
+</div>
+
+<div style = "display:inline-block;vertical-align:top;border:ridge;height:700px;width:194px;">
+    最新ｱﾆﾒのTL</br>
+    <div>
+        <?php
+        require('../tl.php')
+        ?>
+    </div>
+
+</div>
