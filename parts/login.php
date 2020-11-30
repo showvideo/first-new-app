455ceb1631c0464b2a43b0b77ce1f8e19ea0284a
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-01 02:02:36 +0900

    Update login.php

--- parts/login.php
+++ parts/login.php
@@ -7,12 +7,12 @@
 ?>
 
 <div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
-
-    ログイン
-    メールアドレス:<input type="text" name="e-mail">
-    パスワード:<input type="pass" name="pass">
-    <input type="submit" value="ログイン">
-
+    <form action="" method="post">
+        ログイン
+        メールアドレス:<input type="text" name="e-mail">
+        パスワード:<input type="pass" name="pass">
+        <input type="submit" value="ログイン">
+    </form>
 </div>
 
 <div style = "display:inline-block;vertical-align:top;border:ridge;height:700px;width:194px;">
