595b3b432c17a6cac09edc253d2eefc5cf61ba42
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-01 02:08:17 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -10,6 +10,6 @@
 <div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
     <p>ｼｪｱｱﾆ</p></br>
     <a href="../parts/login.php" style="padding-left:490px;">ログイン</a>
-    <a href="signup.php" style="padding-left:15px;">新規登録</a>
+    <a href="../parts/signup.php" style="padding-left:15px;">新規登録</a>
 </div>
 
