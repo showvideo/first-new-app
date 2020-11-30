a3a2e7fa45d336d2837c00771fcbb676b3514046
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-01 01:41:27 +0900

    Update login.php

--- parts/login.php
+++ parts/login.php
@@ -1,1 +1,35 @@
+<?php
+    require('parts/head.php');
+?>
+
+<?php
+    require('func/function.php')
+?>
+
+<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
+    あなたが見たｱﾆﾒを共有しよう</br>
+    ｱﾆﾒで繋がる掲示板</br>
+    ゲーム情報をみんなで交換しよう！
+    友だち募集やゲーム攻略などゲーム情報が満載！</br>
+    <?php
+    echo 'ﾃﾞｲ氏ね';
+    ?>
+    ｱﾆﾒ一覧</br>
+    <div style = "margin-left:10px;">
+        <?php
+        require('parts/title.php')
+        ?>
+    </div>
+
+</div>
+
+<div style = "display:inline-block;vertical-align:top;border:ridge;height:700px;width:194px;">
+    最新ｱﾆﾒのTL</br>
+    <div>
+        <?php
+        require('parts/tl.php')
+        ?>
+    </div>
+
+</div>
 
