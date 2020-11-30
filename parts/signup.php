44aa62d0d575d1e2029dae88d251d3a59e390cb1
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-01 02:07:32 +0900

    Update signup.php

--- parts/signup.php
+++ parts/signup.php
@@ -1,1 +1,26 @@
+<?php
+    require('head.php');
+?>
 
+<?php
+    require('../func/function.php')
+?>
+
+<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
+    <form action="" method="post">
+        ログイン</br>
+        メールアドレス:<input type="text" name="e-mail"></br>
+        パスワード:<input type="pass" name="pass">
+        <input type="submit" value="ログイン">
+    </form>
+</div>
+
+<div style = "display:inline-block;vertical-align:top;border:ridge;height:700px;width:194px;">
+    最新ｱﾆﾒのTL</br>
+    <div>
+        <?php
+        require('tl.php')
+        ?>
+    </div>
+
+</div>
