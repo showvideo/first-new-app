3a44e671b6ab13f1d5ab9ff7adc34c1ae3787d13
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-02 23:43:48 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -3,12 +3,12 @@
 
 <head>
 <meta charset="UTF-8">
-<title>それ見た</title>
+<title>友達募集</title>
 <link rel="stylesheet" href="styles.css"/>
 </head>
 
 <div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
-    <p>ｼｪｱｱﾆ</p></br>
+    <p>ｱﾆﾒ好き―友達募集</p></br>
     <a href="/parts/login.php" style="padding-left:490px;">ログイン</a>
     <a href="/parts/signup.php" style="padding-left:15px;">新規登録</a>
 </div>
