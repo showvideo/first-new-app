b10b6b7fc5d46378aa7c93a626e09381b9085b8a
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-03 00:24:07 +0900

    Update main.php

--- parts/main.php
+++ parts/main.php
@@ -1,10 +1,7 @@
 <?php
-  $db = getDB()
-  
   $sql = 'SELECT name, message FROM user WHERE 1';
-  $stmt = $db->prepare($sql);
+  $stmt = getDB()->prepare($sql);
   $stmt->execute();
-
   $result = $stmt->fetchAll();
   return $result;
 ?>
