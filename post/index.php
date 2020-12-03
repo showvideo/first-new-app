eec97d40a3fcac8847f2270a7a7d3d6cd6c7105e
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-04 07:18:50 +0900

    Update index.php

--- post/index.php
+++ post/index.php
@@ -3,9 +3,8 @@
   require('../func/function.php');
 ?>
 <div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
-ﾊｯｼｭﾀｸﾞ</br>
 
-    <a style="font-size:24px;">最新投稿</a>
+    <a style="font-size:25px;text-align;center;">投稿作成</a>
     <a href="post/post.php"style="padding-left:260px;">投稿作成</a>
     <div style = "margin-left:10px;width:403px;padding-left:20px;">
         <?php
