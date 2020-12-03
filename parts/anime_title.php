5d56735acd3d9ab3d17eabc789d67fada974aa42
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-03 21:34:06 +0900

    Update anime_title.php

--- parts/anime_title.php
+++ parts/anime_title.php
@@ -1,4 +1,5 @@
 <?php
+
   $sql = "SELECT title FROM anime_title WHERE 1";
   $stmt = getDB()->prepare($sql);
   $stmt->execute();
@@ -7,10 +8,7 @@
   
   $anime_titles = array_slice($result,0,100);
   foreach($anime_titles as $anime_title) {
-    echo $anime_title['title']</br>;
+    echo $anime_title['title'];
   }
-  
-  echo "継続は力なり、こつこつと
-  継続すれば、きっと報われるr。。";
 
 ?>
