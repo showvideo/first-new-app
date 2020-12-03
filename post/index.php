fce335eb452356ef5be137bcba9a5fe27d33c139
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-04 07:14:59 +0900

    Update and rename post.php to index.php

---
+++ post/index.php
@@ -0,0 +1,16 @@
+<?php
+  require('../parts/head.php');
+  require('../func/function.php');
+?>
+<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
+ﾊｯｼｭﾀｸﾞ</br>
+
+    <a style="font-size:24px;">最新投稿</a>
+    <a href="post/post.php"style="padding-left:260px;">投稿作成</a>
+    <div style = "margin-left:10px;width:403px;padding-left:20px;">
+        <?php
+        require('../parts/main.php')
+        ?>
+    </div>
+
+</div>
--- post/post.php
+++
@@ -1,4 +0,0 @@
-<?php
-  require("anime/parts/head.php");
-  require("anime/func/function.php");
-?>
