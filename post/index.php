6ede756634ce25b5a8074bc93421448321f75224
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-04 07:26:08 +0900

    Update index.php

--- post/index.php
+++ post/index.php
@@ -2,14 +2,19 @@
   require('../parts/head.php');
   require('../func/function.php');
 ?>
-<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;height:1000px;">
+<div style = "display:inline-block;border:ridge;margin-left:407px;width:450px;">
 
     <a style="font-size:25px;text-align;center;">投稿作成</a>
-    <a href="post/post.php"style="padding-left:260px;">投稿作成</a>
     <div style = "margin-left:10px;width:403px;padding-left:20px;">
-        <?php
-        require('../parts/main.php')
-        ?>
+     
+    <form action="" method="post">
+      <input type="text" name="name">
+      <input type="text" name="old">
+      <input type="text" name="prefecture">
+      <input type="text" name="message">
+      <input type="submit" value="新規投稿">
+    </form>
+
     </div>
 
 </div>
