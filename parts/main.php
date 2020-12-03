72928daed080632cd9d0b09eb5924e5aa71b7555
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-03 18:57:36 +0900

    Update main.php

--- parts/main.php
+++ parts/main.php
@@ -3,8 +3,7 @@
   $stmt = getDB()->prepare($sql);
   $stmt->execute();
   $result = $stmt->fetchAll();
-  echo $reslut;
-  
+
 $titles = array_slice($result,0,100);
 foreach($titles as $title) {
   var_dump($title);
