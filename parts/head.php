a45d2685a3097c872e0f94cdbb77fd701a170047
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-05 18:10:32 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -8,7 +8,7 @@
 </head>
 
 <div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
-    <pstyle = "font-size:24px;padding-left:20px;">アニメ掲示板</p></br>
+    <p style ="font-size:24px;padding-left:20px;">アニメ掲示板</p></br>
     <a href="/parts/login.php" style="padding-left:490px;">ログイン</a>
     <a href="/parts/signup.php" style="padding-left:15px;">新規登録</a>
 </div>
