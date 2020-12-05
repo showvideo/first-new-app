f6f8cde2eef27de599989f82cbf4d7ce43d51456
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-05 10:31:54 +0900

    Update index.php

--- post/index.php
+++ post/index.php
@@ -2,16 +2,16 @@
   require('../parts/head.php');
   require('../func/function.php');
 ?>
-<div style = "display:inline-block;border:ridge;margin-left:420px;width:450px;">
+<div style = "display:inline-block;border:ridge;margin-left:414px;width:450px;">
 
     <a style="font-size:25px;text-align;center;">投稿作成</a>
     <div style = "margin-left:10px;width:403px;padding-left:20px;">
      
     <form action="" method="post">
       名前<input type="text" name="name">
-      歳<input style="width:30px;" type="text" name="old">
-      お住まい<?php require('../parts/prefecturs.php'); ?>
-      コメント<textarea name="message" rows="4" cols="40"><\textarea>
+      歳<input style="width:30px;" type="text" name="old"></br>
+      <a style = "paddimg-top:20px;">お住まい<?php require('../parts/prefecturs.php'); ?></a></br>
+      <a style="padding-top:30px;">コメント<textarea name="message" rows="4" cols="40"placeholder="コメント入力"><\textarea></a></br>
       <input type="submit" value="新規投稿">
     </form>
 
