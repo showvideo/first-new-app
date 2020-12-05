943f451124ff9b07c52d41959df3b4d6594b996a
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-05 10:22:00 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -9,7 +9,7 @@
 
 <div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
     <p>ｱﾆﾒ好き―友達募集</p></br>
-    <a href="/parts/login.php" style="width:60px;padding-left:490px;color:#000;background-color:#fff100;border-bottom: 5px solid #ccc100;">ログイン</a>
+    <a href="/parts/login.php" style="width:60px;padding-left:490px;color:#000;background-color:#fff100;border-bottom: 5px solid #ccc100";>ログイン</a>
     <a href="/parts/signup.php" style="padding-left:15px;color:#000;background-color:#fff100;border-bottom: 5px solid #ccc100;">新規登録</a>
 </div>
 
