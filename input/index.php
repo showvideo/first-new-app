cfb970a1bf2cd2f549f7e31da418d3b5a62839bc
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-08 23:43:23 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -2,4 +2,9 @@
 
 <?php $id = getInfomation(); ?>
 <?php echo $id; ?>
-入浴<?php if(empty($user['visit'])) {?><input type="text"> <?php } ?> 
+来所<?php if(empty($user['visit'])) {?><input type="text" name="visit"> <?php } ?> 
+退所<?php if(empty($user['exit'])) {?><input type="text" name="exit"> <?php } ?> 
+バイタル<?php if(empty($user['vital'])) {?><input type="text" name="vital"> <?php } ?> 
+入浴<?php if(empty($user['bath'])) {?><input type="text" name="bath"> <?php } ?> 
+食事<?php if(empty($user['meal'])) {?><input type="text" name="meal"> <?php } ?> 
+特記事項<?php if(empty($user['notices'])) {?><input type="text" name="notices"> <?php } ?> 
