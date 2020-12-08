38d6bd7076afb6233db3747808af6e055aa52153
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-08 23:32:35 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -1,6 +1,5 @@
 <?php require_once('../func/function.php'); ?>
-<?php var_dump(getInfomation()); ?>
+
 <?php $id = getInfomation(); ?>
-<?php echo $id; ?>
 
 入浴<?php if(empty($user['visit'])) {?><input type="text"> <?php } ?> 
