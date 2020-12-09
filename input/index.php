7792c2937fba679e89fa85e1cda3e14cbcb6da66
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-09 18:53:57 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -2,9 +2,24 @@
 <?php require_once('../func/function.php'); ?>
 <?php $id = getInfomation(); ?>
 <?php echo $id; ?>
+<?php
+  
+  $visit = $_POST['visit'];
+  $exit = $_POST['exit'];
+  $vital = $_POST['vital'];
+  $bath = $_POST['bath'];
+  $meal = $_POST['meal'];
+  $notiices = $_POST['notices'];
+
+?>
 <form action=""method="post">
 来所<?php if(empty($user['visit'])) {?><input type="text" name="visit"> <?php } ?> 
-<?php /* if(isset($_POST['submit'])) { $sql = 'INSERT INTO user(visit) VALUES(:visit) */?>
+<?php if(isset($_POST['submit'])) { 
+  $sql = 'INSERT INTO user(visit) VALUES(:visit);' 
+  $stmt = getDB()->prepare($sql);
+  $stmt->bindParam(:visit, $visit, PDO_PARAM_STR);
+  $stmt->execute();
+?>
 退所<?php if(empty($user['exit'])) {?><input type="text" name="exit"> <?php } ?> 
 バイタル<?php if(empty($user['vital'])) {?><input type="text" name="vital"> <?php } ?> 
 入浴<?php if(empty($user['bath'])) {?><input type="text" name="bath"> <?php } ?> 
