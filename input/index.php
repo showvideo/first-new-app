fc8af809f2d44c6b43d624166cf7060ef017d031
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-10 02:05:34 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -13,7 +13,7 @@
 
 ?>
 <form action="" method="post">
-来所<?php if(!isset($user['visit'])) { echo null; } else {?> <input type="text" name="visit"> <?php } ?> 
+<?php if(!isset($user['visit'])) { echo null; } else {?> 来所:<input type="text" name="visit"> <?php } ?> 
 <?php if(isset($_POST['submit'])) { 
   $sql = 'UPDATE user SET visit=:visit WHERE id= :id';
   $stmt = getDB()->prepare($sql);
@@ -22,11 +22,11 @@
   $stmt->execute();
 }
 ?>
-退所<?php if(isset($user['exit'])) { echo null; } else {?> <input type="text" name="exit"> <?php } ?> 
-バイタル<?php if(isset($user['vital'])) { echo null; } else {?> <input type="text" name="vital"> <?php } ?>
-入浴<?php if(isset($user['bath'])) { echo null; } else {?> <input type="text" name="bath"> <?php } ?>
-食事<?php if(isset($user['meal'])) { echo null; } else {?> <input type="text" name="meal"> <?php } ?>
-特記事項<?php if(isset($user['notices'])) { echo null; } else {?> <input type="text" name="notices"> <?php } ?>
+<?php if(isset($user['exit'])) { echo null; } else {?>退所:<input type="text" name="exit"> <?php } ?> 
+<?php if(isset($user['vital'])) { echo null; } else {?> バイタル:<input type="text" name="vital"> <?php } ?>
+<?php if(isset($user['bath'])) { echo null; } else {?> 入浴:<input type="text" name="bath"> <?php } ?>
+<?php if(isset($user['meal'])) { echo null; } else {?> 食事:<input type="text" name="meal"> <?php } ?>
+<?php if(isset($user['notices'])) { echo null; } else {?>特記事項:<input type="text" name="notices"> <?php } ?>
 <input type="submit" name="submit" value="送信"> 
 </form>
 </body>
