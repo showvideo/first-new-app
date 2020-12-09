459c925fddc0ee30e27e6becd85cc5815875788d
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-10 02:54:48 +0900

    Update index.php

--- information/index.php
+++ information/index.php
@@ -1,5 +1,13 @@
 <div style="border:ridge;width:500px;height:700px;">
      
+<?php
+     $sql = "SELECT id, name, old, pref, date FROM user WHERE 1";
+     $stmt = getDB()->prepare($sql);
+     $stmt->execute();
+     $result = $stmt->fetchAll();
+     foreach($result as $user){
+     $id = $user['id'];   
+?>     
 <?php 
   $name = $_POST['name'];
   echo $namel;
