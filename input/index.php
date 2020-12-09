1c7d60647cd8fca7794142c7af4c085b8d9c85d5
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-10 02:20:24 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -23,10 +23,10 @@
 }
 ?>
 <?php if(isset($user['exit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:500px;">退所:<input type="text" name="exit"></br></div> <?php } ?> 
-<?php if(isset($user['vital'])) { echo null; } else {?> バイタル:<input type="text" name="vital" pattern="^[0~9]+$"></br> <?php } ?>
-<?php if(isset($user['bath'])) { echo null; } else {?> 入浴:<input type="text" name="bath"></br> <?php } ?>
-<?php if(isset($user['meal'])) { echo null; } else {?> 食事:<input type="text" name="meal"></br> <?php } ?>
-<?php if(isset($user['notices'])) { echo null; } else {?>特記事項:<input type="text" name="notices"></br> <?php } ?>
+<?php if(isset($user['vital'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:500px;">バイタル:<input type="text" name="vital" pattern="^[0~9]+$"></div></br> <?php } ?>
+<?php if(isset($user['bath'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:500px;">入浴:<input type="text" name="bath"></br></div> <?php } ?>
+<?php if(isset($user['meal'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:500px;">食事:<input type="text" name="meal"></br></div> <?php } ?>
+<?php if(isset($user['notices'])) { echo null; } else {?><div style="display:inline-block;border:ridge;width:500px;">特記事項:<input type="text" name="notices"></br></div> <?php } ?>
 <input type="submit" name="submit" value="送信"> 
 </form>
 </body>
