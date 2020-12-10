9f0610a4ff407a121ae37bd11910acfbf4498624
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-10 23:20:35 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -14,9 +14,17 @@
 ?>
 <form action="" method="post">
 <?php if(isset($user['visit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br></div> <?php } ?> 
+
+<?php if(isset($user['exit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">退所:<input type="text" name="exit"></br></div> <?php } ?> 
+<?php if(isset($user['vital'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">バイタル:<input type="text" name="vital" pattern="^[0~9]+$"></div></br> <?php } ?>
+<?php if(isset($user['bath'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">入浴:<input type="text" name="bath"></br></div> <?php } ?>
+<?php if(isset($user['meal'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">食事:<input type="text" name="meal"></br></div> <?php } ?>
+<?php if(isset($user['notices'])) { echo null; } else {?><div style="display:inline-block;border:ridge;width:490px;height:600px;">特記事項:<input type="text" name="notices"></br></div> <?php } ?>
+<input type="submit" name="submit" value="送信"> 
+</form>
 <?php if(isset($_POST['submit'])) { 
-  $sql = "INSERT INTO user(visit,exit,vital,bath,meal,notices) VALUES(':visit',':exit',':vital',':bath',':meal',':notices') WHERE id= :'id'";
-  /*$sql = "UPDATE user SET visit=:visit,exit=:exit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= :id";*/
+  /*$sql = "INSERT INTO user(visit,exit,vital,bath,meal,notices) VALUES(':visit',':exit',':vital',':bath',':meal',':notices') WHERE id= :'id'";*/
+  $sql = "UPDATE user SET visit=:visit,exit=:exit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= :id";
   $stmt = getDB()->prepare($sql);
   $stmt->bindParam(':id', $id, PDO::PARAM_STR);
   $stmt->bindParam(':visit', $visit, PDO::PARAM_STR);
@@ -28,11 +36,4 @@
   $stmt->execute();
 }
 ?>
-<?php if(isset($user['exit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">退所:<input type="text" name="exit"></br></div> <?php } ?> 
-<?php if(isset($user['vital'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">バイタル:<input type="text" name="vital" pattern="^[0~9]+$"></div></br> <?php } ?>
-<?php if(isset($user['bath'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">入浴:<input type="text" name="bath"></br></div> <?php } ?>
-<?php if(isset($user['meal'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">食事:<input type="text" name="meal"></br></div> <?php } ?>
-<?php if(isset($user['notices'])) { echo null; } else {?><div style="display:inline-block;border:ridge;width:490px;height:600px;">特記事項:<input type="text" name="notices"></br></div> <?php } ?>
-<input type="submit" name="submit" value="送信"> 
-</form>
 </body>
