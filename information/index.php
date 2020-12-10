735049dfc3727b516462df131a47dc1c56f4a38f
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-10 19:01:09 +0900

    Update index.php

--- information/index.php
+++ information/index.php
@@ -2,18 +2,15 @@
 <?php $id = getInfomation(); ?>
 
 <div style="border:ridge;width:500px;height:700px;">     
-<?php/*
-     $id = $user['id'];   
+<?php
+
      $sql = "SELECT id, name, old, pref, date FROM user WHERE id=:id";
      $stmt = getDB()->prepare($sql);
      $stmt->bindParam(':id', $id, PDO::PARAM_STR);
      $stmt->execute();
      $result = $stmt->fetchAll();
      foreach($result as $user) {
-          $name = $user['name'];
-     }*/
+          echo $user['name'];
+     }
 ?>     
-<?php
-     echo $id;
-?>
 </div>  
