128cba8534d1db1ed17b29044edb33aa330a9305
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-11 10:29:19 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -31,7 +31,7 @@
 }*/
   
   /*$sql = "INSERT INTO user(id,visit,vital,bath,meal,notices) VALUES(:id,:visit,:vital,:bath,:meal,:notices)"; */
-  $sql = "UPDATE user SET visit=:visit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= ':id' ";
+  $sql = "UPDATE user SET id=:id, visit=:visit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= ':id' ";
   $stmt = getDB()->prepare($sql);
   $stmt->bindParam(':id', $id, PDO::PARAM_STR);
   $stmt->bindParam(':visit', $visit, PDO::PARAM_STR);
