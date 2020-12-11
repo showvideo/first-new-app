0da272c0149d63c23aa84603dbbd16adee4f78fe
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-11 14:44:36 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -10,7 +10,7 @@
   $bath = $_POST['bath'];
   $meal = $_POST['meal'];
   $notices = $_POST['notices'];
-
+  echo $id;
 ?>
 <form action="" method="post">
 <?php if(isset($user['visit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br></div> <?php } ?> 
@@ -31,7 +31,7 @@
 }*/
   
   /*$sql = "INSERT INTO user(id,visit,vital,bath,meal,notices) VALUES(:id,:visit,:vital,:bath,:meal,:notices)"; */
-  $sql = "UPDATE user SET id=:id, visit=:visit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= ':id' ";
+  $sql = "UPDATE user SET visit=:visit,vital=:vital,bath=:bath,meal=:meal,notices=:notices WHERE id= ':id' ";
   $stmt = getDB()->prepare($sql);
   $stmt->bindParam(':id', $id, PDO::PARAM_STR);
   $stmt->bindParam(':visit', $visit, PDO::PARAM_STR);
