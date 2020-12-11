cc2888456ab2bb461d0328da2a2ea03ab11b2dce
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-11 15:17:40 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -13,17 +13,22 @@
   echo $id;
 ?>
 <form action="" method="post">
-<?php if(isset($user['visit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br></div> <?php } ?> 
-<input type="submit" name="submit" value="送信"> 　
+<?php if(isset($user['visit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br>
+<input type="submit" name="submit" value="送信"> 　</div> <?php } ?> 
+
 <?php /*if(isset($user['exit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">退所:<input type="text" name="exit"></br></div> <?php } */?> 
-<?php if(isset($user['vital'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">バイタル:<input type="text" name="vital"></div></br> <?php } ?>
-<input type="submit" name="submit1" value="送信"> 　
-<?php if(isset($user['bath'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">入浴:<input type="text" name="bath"></br></div> <?php } ?>
-<input type="submit" name="submit2" value="送信"> 　
-<?php if(isset($user['meal'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">食事:<input type="text" name="meal"></br></div> <?php } ?>
-<input type="submit" name="submit3" value="送信"> 　
-<?php if(isset($user['notices'])) { echo null; } else {?><div style="display:inline-block;border:ridge;width:490px;height:600px;">特記事項:<input type="text" name="notices"></br></div> <?php } ?>
-<input type="submit" name="submit4" value="送信"> 　
+<?php if(isset($user['vital'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">バイタル:<input type="text" name="vital"></div></br>
+<input type="submit" name="submit1" value="送信"><?php } ?>
+
+<?php if(isset($user['bath'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">入浴:<input type="text" name="bath"></br>
+<input type="submit" name="submit2" value="送信"> 　</div> <?php } ?>
+
+<?php if(isset($user['meal'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">食事:<input type="text" name="meal"></br>
+<input type="submit" name="submit3" value="送信"></div> <?php } ?>
+
+<?php if(isset($user['notices'])) { echo null; } else {?><div style="display:inline-block;border:ridge;width:490px;height:600px;">特記事項:<input type="text" name="notices"></br>
+<input type="submit" name="submit4" value="送信"> 　</div> <?php } ?>
+
 </form>
 <?php if(isset($_POST['submit'])) { 
   /*$sql = "SELECT id FROM user WHERE id=:id";
