507fda650ca3e4ae557aec2139d0288edfffac74
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-11 16:13:39 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -16,7 +16,7 @@
   echo $id;
 ?>
 <form action="" method="post">
-<?php if(!empty($post)) { ?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br>
+<?php if(empty($post)) { ?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">来所:<input type="text" name="visit"></br>
 <input type="submit" name="submit" value="送信"></div> <?php  } else {echo null;} ?> 
 
 <?php /*if(isset($user['exit'])) { echo null; } else {?> <div style="display:inline-block;border:ridge;width:490px;height:600px;">退所:<input type="text" name="exit"></br></div> <?php } */?> 
