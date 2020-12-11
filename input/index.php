c07101838609979696984d21487950f0bfbebe68
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-11 17:14:00 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -10,7 +10,7 @@
   $bath = $_POST['bath'];
   $meal = $_POST['meal'];
   $notices = $_POST['notices'];
-  $posts = $_POST['posts'];
+  $posts = $_POST['form'];
   $post1 = $_POST['post1'];
   $post2 = $_POST['post2'];
   echo $id;
