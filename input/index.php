5e486a5d3229367c6c6ddb3f67dc8dcf58703162
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-15 22:55:37 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -54,29 +54,10 @@
     ?>
     <h3><?php echo $name."様"; ?></h3></br>
         来所時間:<?php echo $visit; ?></br>
-<form action="" method="post"><input type="submit" name="edit" value="編集"></form>
+<form action="../edit/index.php" method="post"><input type="submit" name="edit" value="編集"></form>
 <input type="submit" name="delete" value="削除">
 </div><?php  } else {echo null;} ?> 
 
-<?php if(isset($_POST['edit'])) { ?>
-    <?php
-       $sql = "SELECT id, name, visit, vital, meal, bath, notices FROM user WHERE id=:id";
-        $stmt = getDB()->prepare($sql);
-        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
-        $stmt->execute();
-        $result = $stmt->fetchAll();
-        foreach($result as $user){
-        $id = $user['id'];
-        $name = $user['name'];
-        $visit = $user['visit'];
-        }
-    ?>
-    <h3><?php echo $name."様"; ?></h3></br>
-    来所時間</br>
-    <form action="" method="post">
-    <input type="text" name="visit" style="width:100px;height:40px;" placeholder="<?php echo $visit; ?>"></br>
-<input type="submit" name="edit1" value="編集決定"></form>
-    <?php } ?>
 
 <?php if(isset($_POST['edit1'])) {
   
