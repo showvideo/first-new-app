eaaf0c92142a2b2c57647eb479c353e949659ebc
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-16 20:36:20 +0900

    Update search.php

--- parts/search.php
+++ parts/search.php
@@ -1,5 +1,5 @@
 
-<div style="display:inline-block;border:ridge;width:300px;vertical-align:top;margin-left:6px;">
+<div style="display:inline-block;border:ridge;width:400px;vertical-align:top;margin-left:6px;">
 
     <form action="../search/index.php" method="post"><h4>名前</h4><input type="text" name="name1">
     
