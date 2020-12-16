4bc8f30d48943fe2d6b43bb7962e1a284f874cb6
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-16 10:39:17 +0900

    Update search.php

--- parts/search.php
+++ parts/search.php
@@ -19,3 +19,11 @@
    
    </form>
 </div>
+
+<?php if (isset($_POST['name1'])) {
+    $sql = 'SELECT name FROM user WHERE LIKE'%".$_POST['name1']."%';'
+    $stmt = getDB()->prepare($sql);
+    $result = $stmt->execute();
+    foreach($result as $name) {
+        $name1 = $name['name'];
+}
