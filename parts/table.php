e077db0bf23ef1ee9294933b906816c6649c0806
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-22 11:35:35 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -32,8 +32,8 @@
         <!--来所-->
         <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
         <?php if(empty($visit)){ ?>                       
-            <form name='visitForm' action="../input/index.php/" method="post">                           
-            <a href="../input/index.php/<?php echo $id ?>/" onClick='document.visitForm.submit();' style="opacity:0;">visit</a>
+            <form name='visitForm' action="../input/index.php/<?php echo $id ?>/" method="post">                           
+            <a onClick='document.visitForm.submit();' style="opacity:0;">visit</a>
             <input type="hidden" name="visitname" value="1">
             </form>
             <?php }else { ?>
