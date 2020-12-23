ef965ab6ee2bb7327437668bb4dd4c0eed5fa26d
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-24 02:03:33 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -40,9 +40,9 @@
         <!--来所-->
         <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
         <?php if(!isset($visit)){ ?>                       
-            <form action="../input/index.php/<?php echo $id ?>/" method="post"><a href="../input/index.php/<?php echo $id ?>/?visitTime=1" style="opacity:0;">TIME</a></form>                       
+            <form action="../../input/index.php/<?php echo $id ?>/" method="post"><a href="../../input/index.php/<?php echo $id ?>/?visitTime=1" style="opacity:0;">TIME</a></form>                       
             <?php }else { ?>
-            <form action="../input/index.php/<?php echo $id ?>/" method="post"><a href="../input/index.php/<?php echo $id ?>/?visitTime1=1" style="text-decoration:none;color:black;"><?php echo $visit; ?></a></form>
+            <form action="../../input/index.php/<?php echo $id ?>/" method="post"><a href="../../input/index.php/<?php echo $id ?>/?visitTime1=1" style="text-decoration:none;color:black;"><?php echo $visit; ?></a></form>
         <?php } ?>
         </td>
         
