aa73aa1e0b8897a570cfe7b78b2135933154dab9
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-23 23:11:03 +0900

    Update search.php

--- parts/search.php
+++ parts/search.php
@@ -13,11 +13,11 @@
         <input type="text" name="search_exit" style="width:30px;height:40px;margin-left:20px;">時<input type="text" name="visit" style="width:30px;height:40px;">分
         
       <h4>バイタル</h4>
-        <a style="margin-left:20px;">最高血圧(mmhg)</a><input type="text" name="vital1" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="vital1" style="width:40px;height:50px;"></br>
+        <a style="margin-left:10px;">最高血圧(mmhg)</a><input type="text" name="vital1" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="vital1" style="width:40px;height:50px;"></br>
       
-        <a style="margin-left:20px;">最低血圧(mmhg)</a><input type="text" name="vital2" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="vital2" style="width:40px;height:50px;"></br>
+        <a style="margin-left:10px;">最低血圧(mmhg)</a><input type="text" name="vital2" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="vital2" style="width:40px;height:50px;"></br>
         
-        <a style="margin-left:20px;">脈拍(拍/分)</a><input type="text" name="vital3" style="width:40px;height:50px;margin-left:63px;">～<input type="text" name="vital3" style="width:40px;height:50px;">
+        <a style="margin-left:10px;">脈拍(拍/分)</a><input type="text" name="vital3" style="width:40px;height:50px;margin-left:63px;">～<input type="text" name="vital3" style="width:40px;height:50px;">
         
       <h4>入浴ありで絞り込む</h4>
         <input type="radio" name="bath1" value="はい" style="margin-left:20px;"> はい
