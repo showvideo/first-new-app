81a0bf954ca749c5092289154f3000b9a6b4c309
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-23 20:12:26 +0900

    Update index.php

--- search/index.php
+++ search/index.php
@@ -22,8 +22,8 @@
                 
             $sql = "SELECT id, name, visit, exits, vital, meal, bath, notices FROM user WHERE (name LIKE '%".':name'."%') and (visit=:visit)";
             $stmt = getDB()->prepare($sql);
-            $stmt->bindParam(':visit', searchName($_POST['search_visit']), PDO::PARAM_STR);
             $stmt->bindParam(':name', searchName($_POST['search_name']), PDO::PARAM_STR);    
+            $stmt->bindParam(':visit', searchName($_POST['search_visit']), PDO::PARAM_STR);
             $stmt->execute();
             $result = $stmt->fetchAll();
             foreach($result as $user){
