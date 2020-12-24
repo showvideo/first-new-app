510eb46e8266d04eeb1071360b1fd85e98ce29ac
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-24 18:45:15 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -15,6 +15,8 @@
   $visitime = $_POST['visitime'];
   $visitime = $_GET['visitime'];
   $visitime1 = $_GET['visitime1'];
+  $exitime = $_GET['exitime'];
+  $exitime1 = $_GET['exitime1'];
 ?>
 
 <?php /*来所*/ ?>
@@ -56,6 +58,53 @@
     ?>
     <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
     <p style="padding-left:40px;font-size:20px;">来所時間</p></br>
+    <input type="text" placeholder="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:40px;color:black;font-weight:bold;">:
+    <input type="text" placeholder="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
+    <form action="" method="post">
+    <input type="submit" name="edit" value="編集"></br>
+    <a href="" name="delete" value="削除" style="margin-left:40px;">削除はこちらをクリック</a>
+    <input type="submit" name="back" value="一覧へ戻る" style="position:absolute;bottom:9px;"></form>
+    </div><?php  } else {echo null;} ?> 
+
+<?php /*退所*/ ?>
+
+<?php if(!empty($exitime)) { ?> <div style="display:inline-block;border:ridge;width:567px;height:779px;">
+    <?php
+        $sql = "SELECT id, name, visit, vital, meal, bath, notices FROM user WHERE id=:id";
+        $stmt = getDB()->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        foreach($result as $user){
+        $id = $user['id'];
+        $name = $user['name'];
+        }
+    ?>
+    <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
+    <p style="padding-left:40px;font-size:20px;">退所時間</p></br>
+    <form action="" method="post">
+        <input type="text" style="width:44px;height:55px;margin-left:40px;">:
+        <input type="text" style="width:44px;height:55px;">
+        <input type="submit" name="submit" value="入力">
+        <input type="submit" name="back" value="一覧へ戻る" style="position:absolute;bottom:9px;">
+    </form>
+    <?php  } else {echo null;} ?> 
+
+<?php if(isset($exitime1)) { ?> <div style="display:inline-block;border:ridge;width:567px;height:779px;position:relative;">
+   <?php
+       $sql = "SELECT id, name, visit, vital, meal, bath, notices FROM user WHERE id=:id";
+        $stmt = getDB()->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        foreach($result as $user){
+        $id = $user['id'];
+        $name = $user['name'];
+        $visit = $user['visit'];
+        }
+    ?>
+    <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
+    <p style="padding-left:40px;font-size:20px;">退所時間</p></br>
     <input type="text" placeholder="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:40px;color:black;font-weight:bold;">:
     <input type="text" placeholder="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
     <form action="" method="post">
