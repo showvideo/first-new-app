7b50765b9c305a2526977888f8ab370d024f7a4d
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-24 21:36:42 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -33,9 +33,9 @@
         
     ?>
     <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
-    <p style="padding-left:40px;font-size:20px;">来所時間</p></br>
+    <p style="margin-left:40px;font-size:20px;">来所時間</p></br>
     <form action="" method="post">
-        <input type="text" style="width:44px;height:55px;margin-left:40px;">:
+        <input type="text" style="width:44px;height:55px;margin-left:60px;">:
         <input type="text" style="width:44px;height:55px;">
         <input type="submit" name="submit" value="入力">
         <input type="submit" name="back" value="一覧へ戻る" style="position:absolute;bottom:9px;">
@@ -44,7 +44,7 @@
         }
     } else {echo null;} ?> 
 
-<?php if(isset($visitime1)) { ?> <div style="display:inline-block;border:ridge;width:567px;height:779px;position:relative;">
+<?php if(isset($visitime1)) { ?> <div style="display:inline-block;border:ridge;width:367px;height:579px;position:relative;">
    <?php
        $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
         $stmt = getDB()->prepare($sql);
@@ -58,8 +58,8 @@
         }
     ?>
     <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
-    <p style="padding-left:40px;font-size:20px;">来所時間</p></br>
-    <input type="text" value="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:40px;color:black;font-weight:bold;">:
+    <a style="margin-left:40px;font-size:20px;">来所時間</a></br>
+    <input type="text" value="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:60px;color:black;font-weight:bold;">:
     <input type="text" value="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
     <form action="" method="post">
     <input type="submit" name="edit" value="編集"></br>
@@ -92,7 +92,7 @@
     </form>
     <?php  } else {echo null;} ?> 
 
-<?php if(isset($exitime1)) { ?> <div style="display:inline-block;border:ridge;width:567px;height:779px;position:relative;">
+<?php if(isset($exitime1)) { ?> <div style="display:inline-block;border:ridge;width:367px;height:579px;position:relative;">
    <?php
        $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
         $stmt = getDB()->prepare($sql);
@@ -106,8 +106,8 @@
         }
     ?>
     <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
-    <p style="padding-left:40px;font-size:20px;">退所時間</p></br>
-    <input type="text" placeholder="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:40px;color:black;font-weight:bold;">:
+    <p style="margin-left:40px;font-size:20px;">退所時間</p></br>
+    <input type="text" placeholder="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:60px;color:black;font-weight:bold;">:
     <input type="text" placeholder="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
     <form action="" method="post">
     <input type="submit" name="edit" value="編集"></br>
