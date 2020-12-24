36429bf754498157e31fe8e7ba68f06c2d44a81b
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-24 21:42:19 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -57,13 +57,13 @@
         $visit = $user['visit'];
         }
     ?>
-    <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
+    <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3><a href="" name="delete" value="削除" style="margin-left:200px;">削除はこちらをクリック</a></br>
     <a style="margin-left:40px;font-size:20px;padding-bottom:10px;">来所時間</a></br>
     <input type="text" value="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:47px;color:black;font-weight:bold;">:
     <input type="text" value="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
     <form action="" method="post">
     <input type="submit" name="edit" value="更新" style="margin-left:299px;margin-bottom:40px;"></br>
-    <a href="" name="delete" value="削除" style="margin-left:40px;">削除はこちらをクリック</a>
+    
     <input type="submit" name="back" value="一覧へ戻る" style="position:absolute;bottom:9px;"></form>
     </div><?php  } else {echo null;} ?> 
 
