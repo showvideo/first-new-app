d6b72f96f63e4dd7bc876605f48699ea28f5b650
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-25 12:08:07 +0900

    Update index.php

--- search/index.php
+++ search/index.php
@@ -16,85 +16,7 @@
         <th style="width:60px;height:35px;border:1px solid #ccc; background:#fff;padding:4px;">食事</th>
         <th style="width:170px;height:35px;border:1px solid #ccc; background:#fff;padding:4px;">特記事項</th>
     </tr>
-        <?php if(!empty($_POST[bath1])) {
-            $sql = "SELECT id, name, visit, exits, maxblood, meal, bath, notices FROM user Wehre bath IS NOT NULL";
-            $stmt = getDB()->preapre($sql);
-            $stmt->execute();
-            $result = $stmt->fetchAll();
-            foreach($result as $user) {
-                $bath = $user['bath']; ?>
-                 <tr>
-        <!--お名前-->
-        <td style="width:100px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-        <?php if(isset($name)){ ?>
-            <form action="information/index.php/<?php echo $id ?>/" method="post">
-            <a href="information/index.php/<?php echo $id ?>/" style="text-decoration:none;color:black;"><?php echo $name; ?></a>
-            </form>    
-        <?php } ?>
-        </td>
-           
-        <!--来所-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-        <?php if(empty($visit)){ ?>                       
-          
-            <a href="../../input/index.php/<?php echo $id; ?>/?visitime=1" style="opacity:0;">TIME</a>                       
-            <?php }else { ?>
-        
-            <a href="../../input/index.php/<?php echo $id; ?>/?visitime1=1" ><?php echo $visit; ?></a>     
-        <?php } ?>
-        </td>
-        
-        <!--退所-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-        <?php if(empty($exits)){ ?>                       
-          
-            <a href="../../input/index.php/<?php echo $id; ?>/?exitime=1" style="opacity:0;">TIME</a>                       
-            <?php }else { ?>
-        
-            <a href="../../input/index.php/<?php echo $id; ?>/?exitime1=1" ><?php echo $exits; ?></a>     
-        <?php } ?>
-        </td>
-        
-        <!--最高・最低血圧/脈拍-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-        <?php if(empty($maxblood)){ ?>                       
-          
-            <a href="../../input/index.php/<?php echo $id; ?>/?bloodp=1" style="opacity:0;">TIME</a>                       
-            <?php }else { ?>
-        
-            <a href="../../input/index.php/<?php echo $id; ?>/?bloodp1=1" ><?php echo $maxblood ?></a>     
-        <?php } ?>
-        </td>
-        
-        <!--入浴-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-            <?php echo $bath ?>     
-        </td>
-        
-        
-        <!--食事-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-            <?php echo $meal ?>     
-        </td>
-        
-        
-        <!--特記事項-->
-        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
-        <?php if(empty($notices)){ ?>                       
-          
-            <a href="../../input/index.php/<?php echo $id; ?>/?noticesing=1" style="opacity:0;">TIME</a>                       
-            <?php }else { ?>
-        
-            <a href="../../input/index.php/<?php echo $id; ?>/?noticesing1=1" ><?php echo $notices ?></a>     
-        <?php } ?>
-        </td>
-    </tr>
-            
-        <?php } ?>
-   <?php } ?>  
-   
-    
-          
+     
         <?php
             if(!empty($_POST['search_name'])or($_POST['search_visit'])or($_POST['search_exit'])) {
                 
@@ -256,6 +178,84 @@
             
         <?php } ?>
    <?php } ?>  
+    
+     <?php if(!empty($_POST[bath1])) {
+            $sql = "SELECT id, name, visit, exits, maxblood, meal, bath, notices FROM user Wehre bath IS NOT NULL";
+            $stmt = getDB()->preapre($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            foreach($result as $user) {
+                $bath = $user['bath']; ?>
+                 <tr>
+        <!--お名前-->
+        <td style="width:100px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+        <?php if(isset($name)){ ?>
+            <form action="information/index.php/<?php echo $id ?>/" method="post">
+            <a href="information/index.php/<?php echo $id ?>/" style="text-decoration:none;color:black;"><?php echo $name; ?></a>
+            </form>    
+        <?php } ?>
+        </td>
+           
+        <!--来所-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+        <?php if(empty($visit)){ ?>                       
+          
+            <a href="../../input/index.php/<?php echo $id; ?>/?visitime=1" style="opacity:0;">TIME</a>                       
+            <?php }else { ?>
+        
+            <a href="../../input/index.php/<?php echo $id; ?>/?visitime1=1" ><?php echo $visit; ?></a>     
+        <?php } ?>
+        </td>
+        
+        <!--退所-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+        <?php if(empty($exits)){ ?>                       
+          
+            <a href="../../input/index.php/<?php echo $id; ?>/?exitime=1" style="opacity:0;">TIME</a>                       
+            <?php }else { ?>
+        
+            <a href="../../input/index.php/<?php echo $id; ?>/?exitime1=1" ><?php echo $exits; ?></a>     
+        <?php } ?>
+        </td>
+        
+        <!--最高・最低血圧/脈拍-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+        <?php if(empty($maxblood)){ ?>                       
+          
+            <a href="../../input/index.php/<?php echo $id; ?>/?bloodp=1" style="opacity:0;">TIME</a>                       
+            <?php }else { ?>
+        
+            <a href="../../input/index.php/<?php echo $id; ?>/?bloodp1=1" ><?php echo $maxblood ?></a>     
+        <?php } ?>
+        </td>
+        
+        <!--入浴-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+            <?php echo $bath ?>     
+        </td>
+        
+        
+        <!--食事-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+            <?php echo $meal ?>     
+        </td>
+        
+        
+        <!--特記事項-->
+        <td style="width:60px;height:34px;border:1px solid #ccc;background:#fff;padding:4px;">
+        <?php if(empty($notices)){ ?>                       
+          
+            <a href="../../input/index.php/<?php echo $id; ?>/?noticesing=1" style="opacity:0;">TIME</a>                       
+            <?php }else { ?>
+        
+            <a href="../../input/index.php/<?php echo $id; ?>/?noticesing1=1" ><?php echo $notices ?></a>     
+        <?php } ?>
+        </td>
+    </tr>
+            
+        <?php } ?>
+   <?php } ?>  
+   
 </table>
     <input type="button" onclick="location.href='https://animech2.herokuapp.com/'" value="一覧へ戻る">
 </div>
