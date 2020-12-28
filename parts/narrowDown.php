4f821be561c9b15b57ceb8367e3b27eb19aed994
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-29 02:54:49 +0900

    Update narrowDown.php

--- parts/narrowDown.php
+++ parts/narrowDown.php
@@ -15,7 +15,7 @@
         <option value="11:00">11時00分</option>
         </select>
         
-    <h4>退所</h4>
+    <p style="text-align:center;border-bottom:ridge;width:80px;border-right:ridge;margin-top:0;border-top:300px;border-top:ridge;margin-top:30px;">退所</p>
         <select name="search_exit" style="margin-left:20px;">
         <option value="選択なし">選択なし</option>
         <option value="13:00">13時00分</option>
@@ -24,22 +24,22 @@
         <option value="14:30">14時30分</option>
         </select>
         
-      <h4>バイタル</h4>
+      <p style="text-align:center;border-bottom:ridge;width:80px;border-right:ridge;margin-top:0;border-top:300px;border-top:ridge;margin-top:30px;">バイタル</p>
         <a style="margin-left:10px;">最高血圧</a><input type="text" name="maxblo" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="maxblo1" style="width:40px;height:50px;">(mmhg)</br>
       
         <a style="margin-left:10px;">最低血圧</a><input type="text" name="maxblo2" style="width:40px;height:50px;margin-left:20px;">～<input type="text" name="maxblo3" style="width:40px;height:50px;">(mmhg)</br>
         
         <a style="margin-left:10px;">脈拍</a><input type="text" name="maxblo4" style="width:40px;height:50px;margin-left:50px;">～<input type="text" name="maxblo5" style="width:40px;height:50px;">(拍/分)
         
-      <h4>入浴ありで絞り込む</h4>
+      <p style="text-align:center;border-bottom:ridge;width:80px;border-right:ridge;margin-top:0;border-top:300px;border-top:ridge;margin-top:30px;">入浴ありで絞り込む</p>
         <input type="radio" name="bath1" value="yes" style="margin-left:20px;"> はい
         <input type="radio" value="no"> いいえ
         
-      <h4>食事ありで絞り込む</h4>
+      <p>食事ありで絞り込む</p>
         <input type="radio" name="meal1" value="はい" style="margin-left:20px;"> はい
         <input type="radio" value="いいえ" checked> いいえ
       
-      <h4>特記事項ありで絞り込む</h4>
+      <p>特記事項ありで絞り込む</p>
           <input type="radio" name="notices1" value="はい" style="margin-left:20px;"> はい
           <input type="radio" value="いいえ" checked> いいえ    
           <input type="submit" value="絞り込む">
