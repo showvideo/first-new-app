4fc067c205c7fe3ee67e40e8bb51efb963a794a6
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-29 03:01:54 +0900

    Update narrowDown.php

--- parts/narrowDown.php
+++ parts/narrowDown.php
@@ -4,7 +4,7 @@
     <form action="../search/index.php" method="post">
         
         <p style="text-align:center;border-bottom:ridge;width:80px;border-right:ridge;margin-top:0;">名前</p>
-        <input type="text" name="search_name" style="width:100px;margin-top:20px;margin-left:60px;">様
+        <input type="text" name="search_name" style="width:100px;margin-top:20px;margin-left:55px;">様
     
     <p style="text-align:center;border-bottom:ridge;width:80px;border-right:ridge;margin-top:0;border-top:300px;border-top:ridge;margin-top:30px;">来所時間</p>        
         <select name="search_visit" style="margin-left:20px;">
