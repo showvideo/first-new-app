b3f115b36b25b32fd58ac068a962c9f65631a6fe
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-30 01:48:18 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -37,7 +37,7 @@
     <form action="" method="post">
     <a style="border:ridge;">来所時間</a></br>
     
-    <div style="width:325px;border:ridge;margin:0;"></br>
+    <div style="width:300px;border:ridge;margin:0;"></br>
         <p style="padding-left:20px;font-size:20px;margin:0;"><?php echo $name."様"; ?></p></br>
 
         <a style="padding-left:20px;">来所時間を選択してください。</br>
@@ -47,7 +47,7 @@
         <input type="submit" value="09:30" style="margin-left:20px;">
         <input type="submit" value="09:30">
         <input type="submit" value="09:30"></br>
-        <input type="submit" value="09:30" style="margin-left:20px;">
+        <input type="submit" value="09:30" style="margin-left:20px;margin-bottom:40px;">
     </form>
     </div>
 </div>
