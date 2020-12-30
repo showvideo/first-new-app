a9b4a9cf0dc29c1b54c8121a5944d63774362520
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-30 19:23:08 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -70,14 +70,27 @@
         $visit = $user['visit'];
         }
     ?>
-    <div style="display:inline-block;width:315px;vertical-align:top;margin-left:6px;">
+        <div style="border:ridge;width:300px;height:400px;vertical-align:top;margin-left:6px;">
 
-    <form action="" method="post">
-    <a style="border:ridge;font-size:19px;padding:6px;margin:0;">来所時間</a></br>
-    
-    <div style="width:300px;border:ridge;margin-top:6px;"></br>
-        <a style="margin-left:20px;font-size:30px;margin-bottom:10px;text-decoration:underline;"><?php echo $name."様"; ?></a></br>
+        <form action="" method="post">
+            <p>来所時間</p>
+            <a style="padding-left:200px;padding-bottom:50px;"><?php echo $name."様"; ?></a></br>
+            <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">只今設定されている時間</a></br>
+            <?php echo $visit; ?></br>
+        <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">来所時間を選択してください。</a></br>
 
+            <select name="visit_time" style="width:100px;height:30px;margin-left:40px;">
+                <option value="">選択しない</option>
+                <option value="09:30">09:30</option>
+                <option value="10:00">10:00</option>
+                <option value="10:30">10:30</option>
+                <option value="11:00">11:00</option>
+                <option value="11:30">11:30</option>
+                <option value="12:00">12:00</option>
+            </select></p>
+        <input type="submit" value="一覧へ戻る" style="margin-left:120px;margin-top:190px;">
+        </form>
+    </div>
         
     </form>
 
