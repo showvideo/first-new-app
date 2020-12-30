ace0788f736d3f6b5aba9a7489567719dd1c143c
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2020-12-30 19:33:03 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -137,7 +137,7 @@
 
 <?php if(isset($exitime1)) { ?> <div style="display:inline-block;border:ridge;width:367px;height:579px;position:relative;">
    <?php
-       $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
+       $sql = "SELECT * FROM user WHERE id=:id";
         $stmt = getDB()->prepare($sql);
         $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->execute();
@@ -145,17 +145,33 @@
         foreach($result as $user){
         $id = $user['id'];
         $name = $user['name'];
-        $visit = $user['visit'];
+        $exit = $user['exits'];
         }
-    ?>
-    <h3 style="padding-left:20px;font-size:30px;"><?php echo $name."様"; ?></h3></br>
-    <p style="margin-left:40px;font-size:20px;">退所時間</p></br>
-    <input type="text" placeholder="<?php echo substr($visit, 0, 2); ?>" style="width:44px;height:55px;margin-left:60px;color:black;font-weight:bold;">:
-    <input type="text" placeholder="<?php echo substr($visit, 3, 5); ?>" style="width:44px;height:55px;">
-    <form action="" method="post">
-    <input type="submit" name="edit" value="編集"></br>
-    <a href="" name="delete" value="削除" style="margin-left:40px;">削除はこちらをクリック</a>
-    <input type="submit" name="back" value="一覧へ戻る" style="position:absolute;bottom:9px;"></form>
+    ?>        
+        <div style="border:ridge;width:300px;height:400px;vertical-align:top;margin-left:6px;">
+
+        <form action="" method="post">
+            <p>来所時間</p>
+            <a style="padding-left:200px;padding-bottom:50px;"><?php echo $name."様"; ?></a></br>
+            <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">只今設定されている時間</a></br>
+            <a style="padding-left:40px;font-size:17px;"><?php echo $exit; ?></a></br>
+        <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">退所時間を選択してください。</a></br>
+
+            <select name="visit_time" style="width:100px;height:30px;margin-left:40px;">
+                <option value="">選択しない</option>
+                <option value="09:30">09:30</option>
+                <option value="10:00">10:00</option>
+                <option value="10:30">10:30</option>
+                <option value="11:00">11:00</option>
+                <option value="11:30">11:30</option>
+                <option value="12:00">12:00</option>
+            </select></p>
+        <input type="submit" value="一覧へ戻る" style="margin-left:110px;margin-top:160px;">
+
+    </div>
+        
+    </form>
+
     </div><?php  } else {echo null;} ?> 
 
 <?php /*血圧*/ ?>
