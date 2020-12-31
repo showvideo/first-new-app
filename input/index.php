70b16d2699d693005ad27e1e13d66e9e6fd8ec53
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-01 01:05:50 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -22,24 +22,23 @@
 
 <?php if(!empty($visitime)) { ?> 
     <?php
-    
-        $db = getDB();
-        $stt = $db->preapare('SELECT * FROM user WHERE id = :id');
-        $stt->bindValue(':id', $id, PDO::PARAM_STR);
-        $stt->execute();
+        $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
+        $stmt = getDB()->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->execute();
         $result = $stmt->fetchAll();
         foreach($result as $user){
         $id = $user['id'];
         $name = $user['name'];
         }
     ?>
-    <div style="border:ridge;width:300px;height:400px;vertical-align:top;margin-left:6px;">
+        <div style="border:ridge;width:300px;height:400px;vertical-align:top;margin-left:6px;">
 
         <form action="" method="post">
             <p>来所時間</p>
             <a style="padding-left:200px;padding-bottom:50px;"><?php echo $name."様"; ?></a></br>
             
-        <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">来所時間を選択してください。</a></br>
+        <a style="padding-left:30px;font-size:13px;margin-top:50px;padding-top:50px;">来時間を選択してください。</a></br>
 
             <select name="visit_time" style="width:100px;height:30px;margin-left:40px;">
                 <option value="">選択しない</option>
@@ -49,16 +48,11 @@
                 <option value="11:00">11:00</option>
                 <option value="11:30">11:30</option>
                 <option value="12:00">12:00</option>
-            </select>
+            </select></p>
         <input type="submit" value="一覧へ戻る" style="margin-left:110px;margin-top:190px;">
         </form>
     </div>
-
-    <?php  
-        
-    } else {echo null;} ?> 
-
-<?php if(isset($visitime1)) { ?> 
+    <?php  } else {echo null;} ?> <?php if(isset($visitime1)) { ?> 
    <?php
        $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
         $stmt = getDB()->prepare($sql);
