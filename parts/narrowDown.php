35e19a228e593625edb7cd0c98fdc2f08385f3af
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-04 20:15:30 +0900

    Update narrowDown.php

--- parts/narrowDown.php
+++ parts/narrowDown.php
@@ -3,14 +3,14 @@
 <div style="display:inline-block;width:250px;vertical-align:top;margin-left:6px;">
 
     <form action="../search/index.php" method="post">
-    <div style="border:ridge;padding:4px;margin:0;">来所時間</div>
+    <div style="border:ridge;padding:4px;margin:0;width:60px;hegiht:200px;">来所時間</div>
     
-    <div style="width:200px;border:ridge;margin:0;">時間を入力してください</br>
-        <input type="text" name="search_visit" style="width:90px;">
+    <div style="width:200px;border:ridge;margin:0;margin-left:20px;">時間を入力してください</br>
+        <input type="text" name="search_visit" style="width:100px;margin-left:40px;">
         <input type="submit" name="vsearch_submit" value="絞り込み">
     </div>
     
-    <div style="border:ridge;margin-top:20px;padding:4px;">退所時間</div>
+    <div style="border:ridge;margin-top:20px;padding:4px;width:60px;">退所時間</div>
         <input type="text" name="search_visit" style="width:90px;margin-left:20px;">
         <input type="submit" name="esearch_submit"> 
     </div>
