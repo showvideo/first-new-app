5197ae67dc7f6af31d331a63affc96f0f0b01128
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 11:48:41 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -6,7 +6,7 @@
 <meta charset="UTF-8">
 <title>名簿、一覧</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="../public/styles.css">
 </head>
 
 
