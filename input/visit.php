e602893aa7d3b612607002622c66a660ad977d43
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 20:52:40 +0900

    Update visit.php

--- input/visit.php
+++ input/visit.php
@@ -2,11 +2,31 @@
 <?php require_once('../common/function.php'); ?>
 <?php require_once('../parts/header.php'); ?>
 <?php $id = getInfomation(); ?>
-hello
-<?php require_once('../input/visit.php'); ?>
-<?php require_once('../input/exit.php'); ?>
-<?php require_once('../input/blood.php'); ?>
-<?php require_once('../input/notices.php'); ?>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">入所時間</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">時間を入力してください</small>
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+<?php
+try {
+    $sql = "UPDATE users SET visit = :visit where id = :id";
+    $stt = getDB()->prepare($sql)
+    $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+    $stmt->bindParam(':visit', $visit, PDO::PARAM_STR);
+    $stt->execute();
+    header('Location: https://animech2.herokuapp.com/');
+} catch (PDOException $e) {
+    echo "ｴﾗｰﾒｯｾｰｼﾞ:{$e->getMessage()}";
+}
+?>
 <?php if(!empty($visitime)) { ?> 
     <?php
         $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
