8525198fcedcd1971c4aa83a1ed87edaccf1d91d
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 11:33:32 +0900

    Update head.php

--- parts/head.php
+++ parts/head.php
@@ -3,15 +3,9 @@
 
 <head>
 <meta charset="UTF-8">
-<title>友達募集</title>
-<link rel="stylesheet" href="styles.css"/>
+<title>名簿、一覧</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="styles.css">
 </head>
 
-<div style = "margin-left:auto;margin-right:auto;border:ridge;width:654px;">
-    <p style ="font-size:24px;padding-left:20px;margin:0;">アニメ掲示板</p></br>
-    <a>あなたが見たアニメを共有しよう</a></br>
-    <a>あなたのおすすめのアニメをみんなに教えてね</a>
-    <a href="/parts/login.php" style="padding-left:490px;">ログイン</a>
-    <a href="/parts/signup.php" style="padding-left:15px;">新規登録</a>
-</div>
 
