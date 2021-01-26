500eee85667b80e8c7de528a5cc44b7fdc8707f8
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 21:56:30 +0900

    Update memo.php

--- parts/memo.php
+++ parts/memo.php
@@ -1,6 +1,7 @@
-<div style="width:200px;display:inline-block;vertical-align:center;border:solid 1px;margin-top:50px;">
 スタッフメモ
-<?php echo $_POST['note']; ?>
+<div style="width:200px;display:inline-block;vertical-align:center;border:solid 1px;margin-top:50px;">
+
+・<?php echo $_POST['note']; ?>
 <form action="" method="post">
 <input name="note" type='text'>
 <input type='submit' value='入力'>
