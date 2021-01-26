8b31445c21ee5f37a63446bfebe3b32116a7bc6a
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 21:05:31 +0900

    Update visit.php

--- input/visit.php
+++ input/visit.php
@@ -5,12 +5,8 @@
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">入所時間</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">時間を入力してください</small>
-  </div>
-  <div class="form-group form-check">
-    <input type="text" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
