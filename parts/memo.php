c4dc943e7804793123b6624a9008a98bd4342e03
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 22:55:59 +0900

    Update memo.php

--- parts/memo.php
+++ parts/memo.php
@@ -8,7 +8,7 @@
 <a style="padding-bottom:200px;"><?php if(isset($_POST['note'])){
         echo "・".$_POST['note']; 
 } ?></a>
-<div style="padding-bottom:200px;">
+<div style="padding-top:200px;">
 <form action="" method="post">
 <input name="note" type='text'>
 <input type='submit' value='入力'>
