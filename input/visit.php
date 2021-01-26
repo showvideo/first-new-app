c29d26fc18d9fbdfaa57b51b0d03301ef76f04d4
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 20:07:24 +0900

    Update visit.php

--- input/visit.php
+++ input/visit.php
@@ -1,4 +1,12 @@
-
+<?php require_once('../parts/head.php'); ?>
+<?php require_once('../common/function.php'); ?>
+<?php require_once('../parts/header.php'); ?>
+<?php $id = getInfomation(); ?>
+hello
+<?php require_once('../input/visit.php'); ?>
+<?php require_once('../input/exit.php'); ?>
+<?php require_once('../input/blood.php'); ?>
+<?php require_once('../input/notices.php'); ?>
 <?php if(!empty($visitime)) { ?> 
     <?php
         $sql = "SELECT id, name, visit, maxblood, meal, bath, notices FROM user WHERE id=:id";
