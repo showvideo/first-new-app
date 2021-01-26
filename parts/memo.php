6b6ec2e1402ab70b0665470729655871825f95b6
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 22:28:16 +0900

    Update memo.php

--- parts/memo.php
+++ parts/memo.php
@@ -1,5 +1,5 @@
 
-<div style="width:200px;display:inline-block;vertical-align:center;border:solid 1px;margin-top:50px;">
+<div style="width:300px;display:inline-block;vertical-align:center;border:solid 1px;margin-top:50px;">
 スタッフメモ</br>
 
 <?php if(!isset($_POST['note'])){
