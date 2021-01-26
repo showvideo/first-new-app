26d2e2abf54deb06a0c188bda92c5ce70df9d18c
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-26 19:52:25 +0900

    Update index.php

--- input/index.php
+++ input/index.php
@@ -4,20 +4,6 @@
 <?php require_once('../common/function.php'); ?>
 <?php $id = getInfomation(); ?>
 
-<?php
-  
-  $visitime = $_GET['visitime'];
-  $visitime1 = $_GET['visitime1'];
-  $exitime = $_GET['exitime'];
-  $exitime1 = $_GET['exitime1'];
-  $bloodp = $_GET['bloodp'];
-  $bloodp1 = $_GET['bloodp1'];
-  $noticesing = $_GET['noticesing'];
-  $noticesing1 = $_GET['noticesing1'];
-
-
-?>
-
 <?php require_once('../input/visit.php'); ?>
 <?php require_once('../input/exit.php'); ?>
 <?php require_once('../input/blood.php'); ?>
