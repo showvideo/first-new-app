17101e18b414ae20143c5ba8034fac68d47f1364
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-01-27 13:20:04 +0900

    Update search.php

--- parts/search.php
+++ parts/search.php
@@ -1,7 +1,7 @@
 <div style="width:200px;vertical-align:top;margin-top:50px;position:absolute;top:600px;left:840px;">
 <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">入所</label>
+    <label for="exampleInputEmail1">血圧</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">時間を入力してください</small>
   </div>
