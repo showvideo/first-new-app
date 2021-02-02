19aa3d407949d60678690534f9af0a172ced5eb0
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 00:27:56 +0900

    Create bloods.php

---
+++ parts/bloods.php
@@ -0,0 +1,15 @@
+<div style="width:250px;border:solid 1px ;position:absolute;top:380px;left:816px;">
+
+<strong style="padding:20px 10px;">検索</strong></br>
+<p style="margin-top:20px;margin-left:20px;">最高血圧</p>
+<form action="note/../search/index.php" method="post">
+
+<input type="text" style="width:70px;margin-left:20px;" name="maxblood">mmgh
+<select name="blood" id=""><option value="up">以上</option><option value="down">以下</option></select></br>
+<input type="submit" value="検索" style="margin-left:20px;margin-top:20px;margin-bottom:20px;">
+
+</form>
+</div>
+
+</form>
+</div>
