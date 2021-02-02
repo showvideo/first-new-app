cfaa1ea6cc248f67e3f2ac5478409f0b5dd86c24
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 00:29:08 +0900

    Update search.php

--- parts/search.php
+++ parts/search.php
@@ -1,14 +1,28 @@
-<div style="width:200px;vertical-align:top;margin-top:50px;position:absolute;top:600px;left:840px;">
-<form>
+<form action = "search/index.php/">
   <div class="form-group">
-    <label for="exampleInputEmail1">血圧</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">時間を入力してください</small>
+    <p style="background-color:whitesmoke;padding: 10px 0;">血圧</p>
+    <div style="padding-left:20px;">
+
+    <p>最高血圧</p>
+    
+    <form action="../search/index.php/" method="post">
+    <input style="width:70px;height:30px;"type="text" class="form-control" name="maxblo">mmgh以上
+    </br>
+
+    <p style="margin-top:30px;">最低血圧</p>
+    
+    <form action="../search/index.php/" method="post">
+    <input style="width:70px;height:30px;"type="text" class="form-control">mmgh以上
+  　</br>
+
+    <p style="margin-top:30px;">脈拍</p>
+    
+    <form action="../search/index.php/" method="post">
+    <input style="width:70px;height:30px;"type="text" class="form-control">mmgh以上
+    <br>
+  <input type="submit">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
         
 </div>
-
-
 
