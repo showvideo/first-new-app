2f91b30b2ec22344c85a8b194d4f23e44ca21b33
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 00:49:19 +0900

    Update visit.php

--- input/visit.php
+++ input/visit.php
@@ -1,26 +1,55 @@
-<?php require_once('../parts/head.php'); ?>
-<?php require_once('../common/function.php'); ?>
-<?php require_once('../parts/header.php'); ?>
+<?php require_once("../parts/head.php"); ?>
+<?php require_once("../parts/header.php"); ?>  
+<?php require_once("../common/function.php"); ?>
 <?php $id = getInfomation(); ?>
-<form action = '' method='post'>
-  <div class="form-group">
-    <label for="exampleInputEmail1">入所時間</label>
-    <input type="text" name="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">時間を入力してください</small>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
 
+<?PHP 
+try {
+
+  $db = getDB();
+  $stt = $db->prepare("select visit from user where id=:id");
+  $stt->bindParam(':id', $id);
+  $stt->execute();
+  while($row = $stt->fetch(PDO::FETCH_ASSOC)) {
+    $visit = $row['visit'];
+    echo $visit;
+  }
+} catch (PDOException $e) {
+  echo "ｴﾗｰﾒｯｾｰｼﾞ:{$e->getMessage()}";
+}
+?> 
+<div style="width:500px;margin-left:250px;margin-top:30px;border:solid 1px gray;padding:40px;display:inline-block;">
+
+<form>
+    <h4>入所時間</h4>
+    
+  <div class="form-group" style="margin-top:40px;">
+    
+  <select name="blood">
+  <option value="09:30">09:30</option>
+  <option value="10:00">10:00</option>
+  <option value="10:30">10:30</option>
+  <option value="11:00">11:00</option>
+  <option value="11:30">11:30</option>
+  </select></p>
+    <small id="emailHelp" class="form-text text-muted">時間を指定してください</small>
+
+  <div class="form-group form-check">
+
+  </div>
+  <button type="submit" class="btn btn-primary" style="margin-left:340px;">送信</button>
+</form>
+</div>
 <?php
-if(isset($_POST['form-control'])){
+if(isset($_POST['name'])){
  
 try {
     $sql = "UPDATE user SET visit = :visit where id = :id";
     $stt = getDB()->prepare($sql);
-    $stt->bindParam(':visit', $_POST['form-control']);
+    $stt->bindParam(':visit', $_POST['name']);
     $stt->bindParam(':id', $id);
     $stt->execute();
-    header('Location: https://animech2.herokuapp.com/');
+    header('Location: http://localhost/note/');
 } catch (PDOException $e) {
     echo "ｴﾗｰﾒｯｾｰｼﾞ:{$e->getMessage()}";
 }
@@ -28,3 +57,10 @@ try {
 }
 
 ?>
+<div>
+
+
+<?php require_once("../parts/numpeople.php"); ?>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
