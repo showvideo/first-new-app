7dd59edf619ba16de2623a8894f838393051b2b9
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 00:46:11 +0900

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -1,18 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">（（施設名））</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <a class="navbar-brand .text-white" href="#">利用者一覧</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="https://animech2.herokuapp.com/">ホーム</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">バイタル</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">日誌作成</a>
+        <a class="nav-link text-white" href="https://animech2.herokuapp.com/">ホーム</a>
       </li>
     </ul>
   </div>
