f38c36d6dcab6d7631288d1e6130aec2af82c8d2
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 00:31:49 +0900

    Create th.php

---
+++ parts/th.php
@@ -0,0 +1,12 @@
+<table border="1" cellspacing="0" style="border-collaspe:collapse;display:inline-block;margin-left:200px;margin-top:40px;vertical-align:top;">
+    <thead>
+    <tr style="background-color:#384D98;">
+        <th style="width:120px;height:35px;border:1px solid #ccc;background:#fff;padding:4px;">お名前</th>
+        <th style="width:60px;height:35px;border:1px solid #ccc;background:#fff;padding:4px;">来所</th>
+        <th style="width:60px;height:35px;border:1px solid #ccc;background:#fff;padding:4px;">退所</th>
+        <th style="width:120px;height:35px;border:1px solid #ccc;background:#fff;padding:4px;">最高/最低/脈拍</th>
+        <th style="width:60px;height:35px;border:1px solid #ccc;background:#fff;padding:4px;">入浴</th>
+        <th style="width:60px;height:35px;border:1px solid #ccc; background:#fff;padding:4px;">食事</th>
+        <th style="width:120px;height:35px;border:1px solid #ccc; background:#fff;padding:4px;">特記事項</th>
+    </tr>
+    </thead>
