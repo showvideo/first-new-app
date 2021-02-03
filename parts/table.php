a9670288d42dd07d7ea1ceb5c9fd8e412f5bae83
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-03 12:29:33 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -20,8 +20,7 @@
 <tbody>
 <tr>
     <!--お名前-->
-    <td onclick="window.location='input/name.php/<?php echo $id ?>/'"  
-     style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $name; ?></a>
+    <td style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;"><?php echo $name; ?></a>
      </td>
            
      <!--来所-->
