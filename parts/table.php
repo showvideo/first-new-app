33949c726a0281411fe189416133e976da5cec0d
TAKERU KITAMURA <68289050+showvideo@users.noreply.github.com>
2021-02-25 18:34:58 +0900

    Update table.php

--- parts/table.php
+++ parts/table.php
@@ -22,7 +22,7 @@
      <!--最高・最低血圧/脈拍-->
      <td onclick="window.location='input/blood.php/<?php echo $id ?>/'"  
      style="height:35px;border:1px solid #ccc;background:#fff;padding:4px;">
-     <?php  echo $maxblood."/".$miniblood."/".$pulse; ?></a>
+     <?php echo $maxblood; ?></a>
      </td>
         
       <!--入浴-->
